2bbe4a0d941648fbdad6d827d3b15e780efbda59
Ana Paula <aninha10.desouza@gmail.com>
2023-03-04 13:54:34 -0300

    componente ItemEstado feito

--- src/components/ItemEstado.js
+++ src/components/ItemEstado.js
@@ -1,13 +1,23 @@
-import {Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet } from "react-native";
 export default function ItemEstado(props) {
-    return (
-        <View style={styles.item}>
-            <Text style={styles.texto}>
-                {props.estado.alimento }
-            </Text>
-        </View>
+  return (
+    <View style={styles.item}>
+      <Text style={styles.texto}>
+        {props.estado.calorias} ({props.estado.alimento})
+      </Text>
+    </View>
+  );
+}
 
-)
-    }
-
-    
+const styles = StyleSheetList.create({
+    item: {
+        backgroundColor: 'blue',
+        padding: 10,
+        width: '100%',
+        alignContent: 'center',
+        alignItems: 'center',
+    },
+    texto: {
+        color: 'white',
+    },
+});
